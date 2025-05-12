2d680889b00753902d04176387e089e3a18adc40
Leonardo Jofré <leonardojavier.jofre.lj@gmail.com>
2025-05-12 18:10:31 -0300

    se agrega notificationBubble a CartWidget.jsx

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,16 +3,40 @@ import carritoIcon from "../assets/icons/carrito.png";
 
 const CartWidget = () => {
   return (
-    <Link to="/cart" >
+    <Link to="/cart" style={styles.cartContainer}>
       <img src={carritoIcon} alt="Carrito de compras" style={styles.cartIcon} />
+      <span style={styles.notificationBubble}>100</span> {}
     </Link>
   );
 };
 
 const styles = {
+  cartContainer: {
+    position: "fixed",
+    top: "10px",
+    right: "10px",
+    zIndex: 1001,
+    display: "flex",
+    alignItems: "center",
+  },
   cartIcon: {
     width: "50px",
     height: "50px",
+  },
+  notificationBubble: {
+    backgroundColor: "#ff0000",
+    color: "#fff",
+    borderRadius: "50%",
+    width: "20px",
+    height: "20px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "bold",
+    position: "absolute",
+    top: "5px",
+    right: "-5px",
   },
 };
 
